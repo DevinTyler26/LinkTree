04d10eb197b0f907a943a5dd9ae4df2d3423ccdb
Devin Cunningham <dtyler.cunningham@gmail.com>
2020-10-09 13:47:10 -0700

    css

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import "../src/main.scss";
+import "../src/css/main.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
