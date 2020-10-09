8184d7fda45f0cb1ac8114577c8377a7e6b90cc2
Devin Cunningham <dtyler.cunningham@gmail.com>
2020-10-09 16:42:13 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
